cba302a4c574f03aa044911e3e3cff64c41fd1d3
carlo <carlomcginley@gmail.com>
2021-04-27 15:47:23 +0100

    implemented layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route } from "react-router-dom";
+
+import AboutMe from "./AboutMe";
+import Education from "./Education";
+import WorkExperience from "./WorkExperience";
+import Projects from "./Projects";
+import ContactMe from "./ContactMe";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route exact path="/" component={AboutMe} />
+      <Route exact path="/Education" component={Education} />
+      <Route exact path="/Experience" component={WorkExperience} />
+      <Route exact path="/Projects" component={Projects} />
+      <Route exact path="/ContactMe" component={ContactMe} />
     </div>
   );
 }
---
+++ src/WorkExperience.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function WorkExperience() {
+  return <h1>Work Experience</h1>;
+}
+
+export default WorkExperience;
