e0ba9b8c1e0628d857332facaf7e2e9c1e5436dc
carlo <carlomcginley@gmail.com>
2021-05-21 09:00:20 +0100

    Added local CV downloading, no longer relying on dryive

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
 import background from "./background.png";
+import CV from "./CV.pdf";
 import Nav from "./Nav";
 
 function AboutMe() {
@@ -45,15 +46,16 @@ function AboutMe() {
                 my skillset after I graduate
               </p>
               <p class="card-text">
-                I have a strong passion for software engineering, and a keen interest in cyber security, with
-                experience in working in software development on a professional,
-                academic and hobbyist level
+                I have a strong passion for software engineering, and a keen
+                interest in cyber security, with experience in working in
+                software development on a professional, academic and hobbyist
+                level
               </p>
               <p class="card-text">Feel Free to take a look around</p>
               <a
                 class="btn btn-primary btn-lg active"
                 target="_blank"
-                href="https://drive.google.com/file/d/1HGC1ZGmNmTFy0dWGtNQ7knZNU78uEqHX/view?usp=sharing"
+                href={CV}
               >
                 Download my CV here
               </a>
