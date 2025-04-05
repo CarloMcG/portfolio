5d548761d72784ce09c4e9a38d670475f832b8ec
Carlo <carlomcginley@gmail.com>
2025-04-05 11:39:43 +0100

    Fixed formatting on the work experience page

--- src/WorkExperience.js
+++ src/WorkExperience.js
@@ -32,33 +32,33 @@ function WorkExperience() {
             </p>
           </Column>
         </Grid >
-        <Column lg={16} md={8} sm={4} style={{ marginTop: '80px' }}>
-          <Grid>
-            <Column md={8} lg={10} sm={4}>
-              <h1 className="bx--type-expressive-heading-01">Intern Software Developer</h1>
-              <h2 className="bx--type-expressive-heading-01">Openjaw Technologies, Galway</h2>
-              <h3 className="bx--type-expressive-heading-01">January 2020 - March 2020</h3>
-            </Column>
-          </Grid>
-        </Column>
-        <Column lg={16} md={8} sm={4} style={{ marginTop: '20px', marginBottom: '20px' }}>
-          <Grid>
-            <Column md={8} lg={10} sm={4}>
-              <p>
-                · Developed front-end web pages using React, and back-end RESTful APIs for a customer service dashboard for use within the aviation industry.</p>
-              <p>
-                · Created unit tests for Java using the JUnit framework, automated front-end tests using Selenium Web
-                Driver, and implemented rigorous regression testing procedures for both a front-end dashboard and an
-                AI chat client that implements IBM Watson.
+      </Column>
+      <Column lg={16} md={8} sm={4} style={{ marginTop: '80px' }}>
+        <Grid>
+          <Column md={8} lg={7} sm={4}>
+            <h1 className="bx--type-expressive-heading-01">Intern Software Developer</h1>
+            <h2 className="bx--type-expressive-heading-01">Openjaw Technologies, Galway</h2>
+            <h3 className="bx--type-expressive-heading-01">January 2020 - March 2020</h3>
+          </Column>
+        </Grid>
+      </Column>
+      <Column lg={16} md={8} sm={4} style={{ marginTop: '20px', marginBottom: '20px' }}>
+        <Grid>
+          <Column md={8} lg={10} sm={4}>
+            <p>
+              · Developed front-end web pages using React, and back-end RESTful APIs for a customer service dashboard for use within the aviation industry.</p>
+            <p>
+              · Created unit tests for Java using the JUnit framework, automated front-end tests using Selenium Web
+              Driver, and implemented rigorous regression testing procedures for both a front-end dashboard and an
+              AI chat client that implements IBM Watson.
 
-              </p>
-              <p>
-                · Participated in weekly planning meetings, and bi-weekly retrospective meetings to plan the future
-                progression of the product, and solve problems as they appeared.
-              </p>
-            </Column>
-          </Grid >
-        </Column >
+            </p>
+            <p>
+              · Participated in weekly planning meetings, and bi-weekly retrospective meetings to plan the future
+              progression of the product, and solve problems as they appeared.
+            </p>
+          </Column>
+        </Grid >
       </Column >
     </Grid >
   );
